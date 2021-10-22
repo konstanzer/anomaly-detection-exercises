c8a3f353d13aa3dda11aa7609d75ce148711ed9e
Steven <steven@Stevens-Air.home>
2021-10-22 12:53:11 -0400

    final nb

--- student-logs-project/prepare.py
+++ student-logs-project/prepare.py
@@ -1,25 +1,50 @@
 import pandas as pd
 import regex as re
+from acquire import acquire_logs
 
-if __name__ == "__main__":
-    
-    df = pd.read_csv("curriculum_logs.csv")
-    
+
+def prepare_logs():
+    """
+    Get all the student logs data.
+    Make datetime index.
+    Drop program ID 4
+    Combine program and cohort name in new column.
+    Delete IPs that look like Codeup admin.
+    Drop extra columns not used in analysis.
+    """
+    df = acquire_logs()
     df['datetime'] =  pd.to_datetime(df.date +" "+ df.time)
     df = df.set_index(df.datetime)
-
-    df=df[df.program_id!=4] #only five, who cares
-    df.program_id = df.program_id.map({1:'PHP', 2:'Java', 3:'Data'}) #map number to program
-    df.name = df.name + "-" + df.program_id #concatenate cohort and program
+    
+    #only five, who cares
+    df=df[df.program_id!=4]
+    #map number to program
+    df.program_id = df.program_id.map({1:'PHP', 2:'Java', 3:'Data'}) 
+    #concatenate cohort and program
+    df.name = df.name + "-" + df.program_id 
+    
+    #dataframe without codeup ips
+    df = df[df.ip.str[:-3] != "97.105.19"]
     
     df = df.drop(columns=['id','cohort_id','deleted_at','slack','created_at',
-                          'updated_at','date','time','datetime','program_id'])
-
+                          'updated_at','time','datetime','program_id'])
     
+    df = prepare_paths(df)
+    
+    return df
+
+
+def prepare_paths(df):
+    """
+    The path column is noisy.
+    I want it to represent the lesson accessed.
+    I use the home directory as the lesson, excluding leading numbers.
+    I then standardize some path names one-by-one.
+    Make new column showing new path counts by cohort.
+    """
     #make path only the top subdirectory
     paths = df.path.str.split("/") #split subdirectories
     home = []
-    
     #to match 3-classifiation, e.g.
     r = re.compile(r'\d*-')
     #make list of top subdirectory
@@ -33,9 +58,8 @@ if __name__ == "__main__":
                 home.append(s)
         except:
             home.append(None)
-
+            
     df['path'] = home
-
     
     #standardizing path names
     df.path[df.path==""]="empty"
@@ -44,14 +68,17 @@ if __name__ == "__main__":
     df.path[df.path == 'capstone'] = 'capstones'
     df.path[df.path == 'javascript'] = 'javascript-i'
     df.path[df.path == 'working-with-time-series-data'] = 'timeseries'
-
-    #dataframe without codeup ips
-    df = df[df.ip.str[:-3] != "97.105.19"]
-
+    
     #make column showing count of that path for that cohort
     df['cohort_path_counts'] = df.groupby(['path','name'])['path'].transform('count')
     
-    df.to_csv("project/prepared.csv")
+    return df
+
+
+if __name__ == "__main__":
+    
+    df = prepare_logs()
+    df.to_csv("prepared.csv")
 
 
 
